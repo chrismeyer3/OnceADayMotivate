032df5e3e8deffab95775b12a27fa3a4e071ef12
Chris Meyer <cmeyerbusiness@gmail.com>
2020-04-12 14:12:03 -0400

    Commit

--- OnceADayMotivate/ViewController.swift
+++ OnceADayMotivate/ViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
 
 class ViewController: UIViewController {
-
+    
+    // A view for when you click on the menu button
+    // ** Consider Renaming **
     var transparentView = UIView()
+    // The table view used in the main menu.
+    // ** Consider Renaming **
     var tableView = UITableView()
     
     // Variable used for the height of the menu. Consider changing variable name.
+    // ** Consider Renaming **
     let height: CGFloat = 250
     
+    // The text values in the menu. Need to rename these when I decide on the actual menu options
     var menuArray = ["Profile", "Favorite", "Notification", "Change Password", "Logout"]
     
+    // This variable displays the image of the day.
     @IBOutlet weak var dailyImage: UIImageView!
+    
+    // This variable displays the quote of the day
+    @IBOutlet weak var dailyQuote: UILabel!
+    
+    // This variable displays today's date
+    @IBOutlet weak var dailyDate: UILabel!
+    
+    // Firebase Reference
+    var db: Firestore!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +46,52 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        // Create a way to show the current date as the text.
+        
+        let todayDate = Date()
+        let todayMonth = todayDate.month
+        let todayDay = todayDate.day
+        dailyDate.text = todayMonth + "\n" + todayDay
+        let todayFullDate = todayDate.fulldate
+        
+        // Firestore things
+        // This calls the Database to grab the current date's quote to set it as the initial loading image
+        db = Firestore.firestore()
+        self.dailyQuote.text = getQuoteFromDate(database: db, dateVal: todayFullDate)
+        
+        //let dbReference = db.collection("dailyQuotes")
+//        dbRef.whereField("date", isEqualTo: todayFullDate).getDocuments() { (querySnapshot, err) in
+//            if err != nil {
+//                    self.dailyQuote.text = "Unable to Get Quote"
+//                } else {
+//                    for document in querySnapshot!.documents {
+//                        let quoteVal = document.get("quote") as! String
+//                        let authorVal = document.get("author") as! String
+//                        self.dailyQuote.text = quoteVal + "\n\n -" + authorVal
+//                    }
+//                }
+//        }
+      //  self.dailyQuote.text = getQuoteFromDate(db: db, dateVal: todayFullDate)
+        // Use this code to get a document based on its specific value
+//        let docRef = db.collection("dailyQuotes").document("85pbGVqSUjUAnS6D4G2x")
+//        docRef.getDocument { (document, error) in
+//             if let document = document, document.exists {
+//                 let docData = document.data()
+//               // self.dailyQuote.text = docData?["quote"] as? String
+//                let quoteVal = docData?["quote"] as! String
+//                let authorVal = docData?["author"] as! String
+//                self.dailyQuote.text = quoteVal + "\n\n -" + authorVal
+//              } else {
+//                 print("Document does not exist")
+//
+//              }
+//        }
+        
+        
     }
     
+    // Function when you click on the menu
     @IBAction func onClickMenu(_ sender: Any) {
         
         let window = UIApplication.shared.keyWindow
@@ -79,4 +143,67 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
+}
+
+extension Date {
+    var month: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM"
+        return dateFormatter.string(from: self)
+    }
+    var day: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d"
+        return dateFormatter.string(from: self)
+    }
+    var fulldate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: self)
+    }
+}
+
+//func getQuoteFromDate(database: Firestore, dateVal: String) -> String {
+//    var dailyQuoteText = "Quote Text Base"
+//    let dbRef = database.collection("dailyQuotes")
+//    dbRef.whereField("date", isEqualTo: dateVal).getDocuments() { (querySnapshot, err) in
+//         if err != nil {
+//                dailyQuoteText = "Unable to Get Quote"
+//                print("Failed to get the item")
+//             } else {
+//                 for document in querySnapshot!.documents {
+//                    print("Checking for date" + dateVal)
+//                    let quoteVal = document.get("quote") as! String
+//                    let authorVal = document.get("author") as! String
+//                    dailyQuoteText = quoteVal + "\n\n -" + authorVal
+//                    print("Writing the quote " + dailyQuoteText)
+//                 }
+//             }
+//     }
+//    print("Returning the quote " + dailyQuoteText)
+//    return dailyQuoteText
+//}
+
+func getQuoteFromDate(database: Firestore, dateVal: String) {
+    let dbRef = database.collection("dailyQuotes")
+    dbRef.whereField("date", isEqualTo: dateVal).getDocuments() { (querySnapshot, err) in
+         if err != nil {
+            var dailyQuoteText = "Unable to Get Quote"
+                print("Failed to get the item")
+             } else {
+                 for document in querySnapshot!.documents {
+                    print("Checking for date" + dateVal)
+                    let quoteVal = document.get("quote") as! String
+                    let authorVal = document.get("author") as! String
+                    dailyQuoteText = quoteVal + "\n\n -" + authorVal
+                    print("Writing the quote " + dailyQuoteText)
+                 }
+             }
+     }
+    print("Returning the quote " + dailyQuoteText)
+    return dailyQuoteText
+}
+
+func quoteFetched() {
+    
 }
